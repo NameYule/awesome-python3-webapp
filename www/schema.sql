6f8abc9e5323847d033e4c9f3719037a122993fd
NameYule <yule.hurrah@gmail.com>
2016-08-25 16:02:54 +0800

    day-07
    add mvc

--- www/schema.sql
+++ www/schema.sql
@@ -6,7 +6,7 @@ create database awesome;
 
 use awesome;
 
-grant select, insert,  update, delete awesome.* to 'www-data'@'localhost'  identified by 'www-data';
+#grant select, insert,  update, delete awesome.* to 'www-data'@'localhost'  identified by 'www-data';
 
 create table users(
 	`id` varchar(50) not null, 
@@ -15,11 +15,11 @@ create table users(
 	`admin` bool not null,
 	`name` varchar(50) not null,
 	`image` varchar(500) not null,
-	`create_at` real not null,
+	`created_at` real not null,
 	unique key `idx_email` (`email`),
-	key `idx_created_at` (`create_at`),
+	key `idx_created_at` (`created_at`),
 	primary key (`id`)
-) engine = innodb default charset==utf8;
+) engine = innodb default charset=utf8;
 
 create table blogs(
 	`id` varchar(50) not null,
@@ -29,8 +29,8 @@ create table blogs(
 	`name` varchar(50) not null,
 	`summary` varchar(200) not null,
 	`content` mediumtext not null,
-	`create_at` real not null,
-	key `idx_created_at`(`create_at`),
+	`created_at` real not null,
+	key `idx_created_at`(`created_at`),
 	primary key (`id`)
 )engine = innodb default charset=utf8;
 
@@ -41,7 +41,8 @@ create table comments(
 	`user_name` varchar(50) not null,
 	`user_image` varchar(500) not null,
 	`content` mediumtext not null,
-	`create_at real not null,
-	key `idx_created_at` (`create_at`),
+	`created_at` real not null,
+	key `idx_created_at` (`created_at`),
 	primary key (`id`)
 ) engine =innodb default charset=utf8;
+
